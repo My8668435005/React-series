28f0259efb04dc7a867f247e84da5fdd3794897a
manish bagul <manishpa4814@gamil.com>
2026-07-14 08:43:42 +0530

    understanding learning component

---
+++ examples/03 - module -3/ConditionalRendering.jsx
@@ -0,0 +1,11 @@
+function Greeting({ isLoggedIn }) {
+  return (
+    <div>
+      {isLoggedIn ? (
+        <h1>Welcome back!</h1>
+      ) : (
+        <h1>Please sign in.</h1>
+      )}
+    </div>
+  );
+}
